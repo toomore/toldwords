07590834c2292f4919e4838ffa83dabac3235841
Toomore Chiang <toomore0929@gmail.com>
2023-04-18 15:51:44 +0800

    Fixed import in __init__ and upgrade packages
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- toldwords/__init__.py
+++ toldwords/__init__.py
@@ -1,2 +1,28 @@
 ''' __init__ '''
-__version__ = '0.2.0'
+__version__ = '0.3.0'
+from .openai import Choice
+from .openai import Message
+from .openai import OpenAIAPI
+from .openai import RespCompletions
+from .openai import Role
+from .openai import TokenUsage
+from .pretalx import Item
+from .pretalx import Pretalx
+from .pretalx import PretalxResponse
+from .utils import DATA2022
+from .utils import DATA2023
+
+__all__ = [
+    "__version__",
+    "Choice",
+    "Message",
+    "OpenAIAPI",
+    "RespCompletions",
+    "Role",
+    "TokenUsage",
+    "Item",
+    "Pretalx",
+    "PretalxResponse",
+    "DATA2022",
+    "DATA2023",
+]
